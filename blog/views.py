eddf7e9ceb81ac739706d13cc05d146d1502e8f7
loveSnowBest <lovesnowbest@gmail.com>
2018-04-20 20:57:05 +0800

    category and archive page

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,get_object_or_404
-from .models import Post
+from .models import Post,Category
 from django.http import HttpResponse
 import markdown
 
@@ -16,3 +16,15 @@ def detail(request,pk):
 									'markdown.extensions.codehilite',
 									'markdown.extensions.toc'])
 	return render(request,'blog/detail.html',context={'post':post})
+
+def archives(request,year,month):
+	post_list=Post.objects.filter(created_time__year=year,
+								  created_time__month=month
+								).order_by('-created_time')
+	return render(request,'blog/index.html',context={'post_list':post_list})
+
+def category(request,pk):
+	cate=get_object_or_404(Category,pk=pk)
+	post_list=Post.objects.filter(category=cate).order_by('-created_time')
+	return render(request,'blog/index.html',context={'post_list':post_list})
+
