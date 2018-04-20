cbbeea0b8c63e5b440a4c7f3dee40eaf72588e69
loveSnowBest <lovesnowbest@gmail.com>
2018-04-20 19:59:46 +0800

    markdown and code highlight supported

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Post
 from django.http import HttpResponse
+import markdown
 
 # Create your views here.
 def index(request):
@@ -9,4 +10,9 @@ def index(request):
 
 def detail(request,pk):
 	post=get_object_or_404(Post,pk=pk)
+	post.body=markdown.markdown(post.body,
+								extensions=[
+									'markdown.extensions.extra',
+									'markdown.extensions.codehilite',
+									'markdown.extensions.toc'])
 	return render(request,'blog/detail.html',context={'post':post})
