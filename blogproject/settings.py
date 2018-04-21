d033a71c2cd61680b94982884c09f370daf7c45f
loveSnowBest <lovesnowbest@gmail.com>
2018-04-21 21:00:29 +0800

    change url patterns

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-g07=9=%7-r2+88y0d#ana3e0&+--a_!1cr=j7yyo2!(q13#q+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost','www.lovesnowbest.ml']
 
