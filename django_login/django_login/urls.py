7357c71b096c237a057cce8a906626ae47df43cf
loveSnowBest <lovesnowbest@gmail.com>
2018-05-08 00:42:01 +0800

    login

--- django_login/django_login/urls.py
+++ django_login/django_login/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from login import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/',views.index),
+    path('login/',views.login),
+    path('register/',views.register),
+    path('logout/',views.logout),
 ]
--- readme.md
+++
@@ -1,5 +0,0 @@
-# Django Blog
-
-This is a simple blog using Django framework. You can view it at <a href="https://www.lovesnowbest.ml">https://www.lovesnowbest.ml</a>.  
-
-And here is the original course <a href="https://www.zmrenwu.com/post/4/">Django博客教程--追梦人物的博客</a>
