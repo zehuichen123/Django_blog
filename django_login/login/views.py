3ee861a17e91efae9844e7795fe3fc64c88ef2d7
loveSnowBest <lovesnowbest@gmail.com>
2018-05-08 14:02:40 +0800

    register page finished

--- django_login/login/views.py
+++ django_login/login/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from . import models
+from . import forms
 # Create your views here.
 
 def index(request):
@@ -8,26 +9,66 @@ def index(request):
     return render(request,'login/index.html')
 
 def login(request):
+    if request.session.get('is_login',None):
+        return redirect('/index/')
     if request.method=="POST":
-        username=request.POST.get('username',None)
-        password=request.POST.get('password',None)
-        if username and password:
-            username=username.strip()
+        login_form=forms.UserForm(request.POST)
+        message="Please check your input"
+        if login_form.is_valid():
+            username=login_form.cleaned_data['username']
+            password=login_form.cleaned_data['password']
             try:
                 user=models.User.objects.get(name=username)
                 if user.password==password:
+                    request.session['is_login']=True
+                    request.session['user_id']=user.id
+                    request.session['user_name']=user.name
                     return redirect('/index/')
                 else:
-                    message="password is not correct!"
+                    message="Password is incorrect!"
             except:
-                message="user is not exists!"
-        return render(request,'login/login.html',{"message":message})
-    return render(request,'login/login.html')
+                message="User is not exists!"
+        return render(request,'login/login.html',locals())
+    login_form=forms.UserForm()
+    return render(request,'login/login.html',locals())
 
 def register(request):
-    pass
-    return render(request,'login/register.html')
+    if request.session.get('is_login',None):
+        return redirect('/index/')
+    if request.method=='POST':
+        register_form=forms.RegisterForm(request.POST)
+        message="Please input content!"
+        if register_form.is_valid():
+            username=register_form.cleaned_data['username']
+            password1=register_form.cleaned_data['password1']
+            password2=register_form.cleaned_data['password2']
+            email=register_form.cleaned_data['email']
+            sex=register_form.cleaned_data['sex']
+            if password1!=password2:
+                message="password is not inconsistent!"
+                return render(request,'login/register.html',locals())
+            else:
+                same_name_user=models.User.objects.filter(name=username)
+                if same_name_user:
+                    message="User has already existed!"
+                    return render(request,'login/register.html',locals())
+                same_email_user=models.User.objects.filter(email=email)
+                if same_email_user:
+                    message="Email has already been used!"
+                    return render(request,'login/register.html',locals())
+                # if all ok, create an account
+                new_user=models.User()
+                new_user.name=username
+                new_user.password=password1
+                new_user.email=email
+                new_user.sex=sex
+                new_user.save()
+                return redirect('/login/')
+    register_form=forms.RegisterForm()
+    return render(request,'login/register.html',locals())
 
 def logout(request):
-    pass
+    if not request.session.get('is_login',None):
+        return redirect('/index/')
+    request.session.flush()
     return redirect('/index/')
