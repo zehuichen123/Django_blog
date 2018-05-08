aaf9270e167eabf29f96042e23708b289582d778
loveSnowBest <lovesnowbest@gmail.com>
2018-05-08 10:59:33 +0800

    login page finished

--- django_login/login/views.py
+++ django_login/login/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from . import models
 # Create your views here.
 
 def index(request):
@@ -7,7 +8,20 @@ def index(request):
     return render(request,'login/index.html')
 
 def login(request):
-    pass
+    if request.method=="POST":
+        username=request.POST.get('username',None)
+        password=request.POST.get('password',None)
+        if username and password:
+            username=username.strip()
+            try:
+                user=models.User.objects.get(name=username)
+                if user.password==password:
+                    return redirect('/index/')
+                else:
+                    message="password is not correct!"
+            except:
+                message="user is not exists!"
+        return render(request,'login/login.html',{"message":message})
     return render(request,'login/login.html')
 
 def register(request):
